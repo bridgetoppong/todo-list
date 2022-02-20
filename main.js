44fc2ddac8deea0c48b40fbca236c4de2c9e1721
bridget <bridgetoppong@rocketmail.com>
2022-02-20 15:57:17 -0500

    project

---
+++ main.js
@@ -0,0 +1,18 @@
+window.addEventListener('load' , () => { 
+    const form = document.querySelector('#new-task-form'); 
+    const input = document.querySelector("#new-task-input"); 
+    const list_el = document.querySelector("#tasks"); 
+
+    form.addEventListener('submit' , (e) => { 
+        e.preventDefault(); 
+
+        const task =input.value; 
+
+        if (!task) { 
+            alert("PLease enter a task!"); 
+        } else { 
+            console.log("Success!"); 
+        }
+    })
+})
+
